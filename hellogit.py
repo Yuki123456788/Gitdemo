ce634da8801e94a78c530d6b6b087d06d2045e85
Yuki <yuki11835@gmail.com>
2022-10-25 14:33:50 +0800

    add placeholder

--- hellogit.py
+++ hellogit.py
@@ -1,8 +1,8 @@
-weight = float(input())
-height = float(input())/100
+weight = float(input("weight: "))
+height = float(input("height: "))/100
 
 while (weight != 0 and height != 0):
     print(f"BMI: {weight/height**2}")
-    weight = float(input())
-    height = float(input())/100
+    weight = float(input("weight: "))
+    height = float(input("height: "))/100
 print("end bmi")
